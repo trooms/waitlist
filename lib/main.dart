584afedbca4f45ccaa6878a97a63f1d3e316e84b
Truman Costello <tcostell@usc.edu>
2021-09-22 04:18:40 -0700

    idek anymore

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'waitlist',
         theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          scaffoldBackgroundColor: Colors.black,
         ),
         home: const Scaffold(
           body: MyHomePage(title: 'waitlist'),
@@ -35,10 +37,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   late String pointerIcon;
   late TextEditingController _controller;
+  late ImageProvider<Object> login;
 
   String loadingIcon = 'images/loading.png';
   String backdrop = 'images/backdrop.png';
-  String login = 'images/login.png';
+  ImageProvider<Object> loginMain = Image.asset(
+    'images/login.png',
+    gaplessPlayback: true,
+  ).image;
+  ImageProvider<Object> loginHint =
+      Image.asset('images/loginHint.png', gaplessPlayback: true).image;
+  ImageProvider<Object> okHover =
+      Image.asset('images/okHover.png', gaplessPlayback: true).image;
   String flickerImg = 'images/flicker.png';
 
   FocusNode myFocusNode = FocusNode();
@@ -53,15 +63,33 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void checkLogin() {
+    if (_controller.text.toLowerCase() == 'mind') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MyDesktop()),
+      );
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     pointerIcon = loadingIcon;
+    login = loginMain;
     Timer.periodic(const Duration(milliseconds: 100), changeBackdrop);
     Timer.periodic(const Duration(seconds: 1), (_) {
       ++timerCount;
     });
     _controller = TextEditingController();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    precacheImage(loginHint, context);
+    precacheImage(loginMain, context);
+    precacheImage(okHover, context);
   }
 
   @override
@@ -94,7 +122,9 @@ class _MyHomePageState extends State<MyHomePage> {
                         decoration: BoxDecoration(
                             image: DecorationImage(
                                 fit: BoxFit.fill,
-                                image: Image.asset(backdrop).image)),
+                                image:
+                                    Image.asset(backdrop, gaplessPlayback: true)
+                                        .image)),
                       ),
                       if (timerCount > 2)
                         Center(
@@ -105,59 +135,101 @@ class _MyHomePageState extends State<MyHomePage> {
                               height: SizeConfig.screenHeight,
                               decoration: BoxDecoration(
                                   image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: Image.asset(login).image)),
+                                      fit: BoxFit.cover, image: login)),
                               child: FittedBox(
                                   fit: BoxFit.cover,
                                   child: SizedBox(
-                                    width: 1125,
-                                    height: 2436,
-                                    child: Center(
-                                      child: Stack(
-                                        children: [
-                                          SizedBox(
-                                            width: 380,
-                                            height: 97,
-                                            child: TextField(
-                                              obscureText: true,
-                                              controller: _controller,
-                                              focusNode: myFocusNode,
-                                              style: const TextStyle(color: Colors.black, fontSize: 30,  ),
-                                              decoration: null,
-                                              cursorColor: Colors.black,
-                                            )
-                                          ),
-                                          Column(
-                                            mainAxisAlignment: MainAxisAlignment.center,
+                                      width: 1125,
+                                      height: 2436,
+                                      child: Center(
+                                          child: Stack(children: [
+                                        Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
                                             children: [
-                                              const SizedBox(
-                                                height: 20,
-                                              ),
+                                              SizedBox(
+                                                  width: 380,
+                                                  height: 40,
+                                                  child: TextField(
+                                                    autofocus: true,
+                                                    obscureText: true,
+                                                    controller: _controller,
+                                                    focusNode: myFocusNode,
+                                                    style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 30,
+                                                    ),
+                                                    decoration: null,
+                                                    cursorColor: Colors.black,
+                                                  )),
                                               Center(
-                                              child: Container(
-                                                width: 300,
-                                                height: 30,
-                                                decoration: BoxDecoration(border: Border.all())
-                                              )
-                                              )
-                                            ]
-                                          )
-                                        ]
-                                      )
-                                    )
-                                  )
-                              )
-                          ),
+                                                  child: SizedBox(
+                                                width: 255,
+                                                height: 35,
+                                                child: Material(
+                                                    color: Colors.transparent,
+                                                    child: InkWell(
+                                                      onTap: () {},
+                                                      onHover: (isHovering) {
+                                                        if (isHovering) {
+                                                          setState(() {
+                                                            login = loginHint;
+                                                          });
+                                                        } else if (login ==
+                                                            loginHint) {
+                                                          setState(() {
+                                                            login = loginMain;
+                                                          });
+                                                        }
+                                                      },
+                                                    )),
+                                              )),
+                                              const SizedBox(height: 20),
+                                            ]),
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            const SizedBox(
+                                              height: 205,
+                                            ),
+                                            Center(
+                                                child: SizedBox(
+                                              width: 180,
+                                              height: 45,
+                                              child: Material(
+                                                  color: Colors.transparent,
+                                                  child: InkWell(
+                                                    onTap: () {
+                                                      checkLogin();
+                                                    },
+                                                    onHover: (isHovering) {
+                                                      if (isHovering) {
+                                                        setState(() {
+                                                          login = okHover;
+                                                        });
+                                                      } else if (login ==
+                                                          okHover) {
+                                                        setState(() {
+                                                          login = loginMain;
+                                                        });
+                                                      }
+                                                    },
+                                                  )),
+                                            ))
+                                          ],
+                                        )
+                                      ]))))),
                         ),
-                      if (flicker)
-                        Container(
-                          width: SizeConfig.screenWidth,
-                          height: SizeConfig.screenHeight,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: Image.asset(flickerImg).image)),
-                        ),
+                      //if (flicker)
+                      //Container(
+                      //width: SizeConfig.screenWidth,
+                      //height: SizeConfig.screenHeight,
+                      //decoration: BoxDecoration(
+                      //image: DecorationImage(
+                      //fit: BoxFit.fill,
+                      //image: Image.asset(flickerImg).image)),
+                      //),
                     ])),
                     if (timerCount < 2)
                       Positioned(
@@ -166,5 +238,54 @@ class _MyHomePageState extends State<MyHomePage> {
                           child: Image.asset(pointerIcon)),
                   ],
                 ))));
+  }
+}
+
+class MyDesktop extends StatefulWidget {
+  const MyDesktop({Key? key}) : super(key: key);
+  @override
+  State<MyDesktop> createState() => _MyDesktopState();
+}
+
+class _MyDesktopState extends State<MyDesktop> {
+  late String backdrop;
+  String newBackdrop = "desktopFilled.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ConstrainedBox(
+        constraints: BoxConstraints.tight(
+            Size(SizeConfig.screenWidth, SizeConfig.screenHeight)),
+        child: Container(
+            color: Colors.white,
+            child: Stack(
+              children: [
+                Center(
+                    child: Stack(children: [
+                  Container(
+                    width: SizeConfig.screenWidth,
+                    height: SizeConfig.screenHeight,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: Image.asset(newBackdrop, gaplessPlayback: true)
+                                .image)),
+                  ), const Text("test")
+                  //Center(
+                    //child: Container(
+                        //margin: EdgeInsets.only(
+                            //top: SizeConfig.screenHeight * 0.075),
+                        //width: SizeConfig.screenHeight,
+                        //height: SizeConfig.screenHeight,
+                        //decoration: BoxDecoration(
+                            //image: DecorationImage(
+                                //fit: BoxFit.cover, image: login
+                            //)
+                        //),
+                    
+                //))
+                ],
+            ))]))));
   }
 }
